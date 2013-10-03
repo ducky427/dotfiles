c9d55e98eaf6992a3daeb81088a1e30237a46caa
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2013-10-03 17:49:07 +0100

    Added some plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,10 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [spyscope "0.1.3"]
                        [criterium "0.4.2"]]
+        :plugins [[lein-cloverage "1.0.2"]
+                  [lein-vanity "0.1.0"]
+                  [lein-ancient "0.4.4"]
+                  [lein-kibit "0.0.8"]]
         :injections [(require '(clojure.tools.namespace repl find))
                      ; try/catch to workaround an issue where `lein repl` outside a project dir
                      ; will not load reader literal definitions correctly:
